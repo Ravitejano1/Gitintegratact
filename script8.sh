a2a42a010138934c57cefc86f95d3cbf7c60fbd5
Ravitejano1 <94282413+Ravitejano1@users.noreply.github.com>
2022-10-25 15:56:20 +0530

    Add files via upload

---
+++ script8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#A simple if statment using
+
+#val=99
+
+echo "Please enter the value:"
+read val
+
+
+if [ $val -eq 100 ]
+then
+        echo "Value is 100"
+else
+        echo "Value is not 100"
+	echo "Value is not equal to 100"
+
+fi
